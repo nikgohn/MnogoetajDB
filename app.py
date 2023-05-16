6afc20576fa0ccd89f6e8568ba7f96b31341f8ee
nikgohn <nikgohn@gmail.com>
2023-05-17 00:11:47 +0300

    expansion of address to table

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from base64 import b64encode
 import os
 
 UPLOAD_FOLDER = '/static/img/uploads'
-ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HomeInfo.db'
@@ -14,39 +14,57 @@ db = SQLAlchemy(app)
 
 
 class Home(db.Model):
+    __tablename__ = "Home_data"
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer, nullable=False)
-    adress = db.Column(db.String(300), nullable=False)
     type = db.Column(db.String(100), nullable=False)
     floor = db.Column(db.Integer, nullable=False)
     porch = db.Column(db.Integer, nullable=False)
     filename = db.Column(db.String(300))
     image = db.Column(db.LargeBinary)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    adress_data = db.relationship('Adress', backref='home')
+
+
+class Adress(db.Model):
+    __tablename__ = "Adress_data"
+    id = db.Column(db.Integer, db.ForeignKey('Home_data.id'), primary_key=True)
+    city = db.Column(db.String(100), nullable=False)
+    district = db.Column(db.String(100), nullable=True)
+    street = db.Column(db.String(300), nullable=False)
+    homenum = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return '<Home %r>' % self.id
 
 
-#@app.route('/')
-#def index():
+# @app.route('/')
+# def index():
 #    return render_template("index.html")
 
 
 @app.route('/AddHome', methods=['POST', 'GET'])
 def addhome():
+
+    current_year = datetime.now().year
+
     if request.method == "POST":
         year = request.form['year']
-        adress = request.form['adress']
         type = request.form['type']
         floor = request.form['floor']
         porch = request.form['porch']
         file = request.files['file']
+        city = request.form['city']
+        district = request.form['district']
+        street = request.form['street']
+        homenum = request.form['homenum']
 
-        home = Home(year=year, adress=adress, type=type, floor=floor, porch=porch, filename=file.filename, image=file.read())
+        home = Home(year=year, type=type, floor=floor, porch=porch, filename=file.filename, image=file.read())
+        adress_data = Adress(city=city, district=district, street=street, homenum=homenum, home=home)
 
         try:
             db.session.add(home)
+            db.session.add(adress_data)
             db.session.commit()
             return redirect('/posts')
 
@@ -54,7 +72,7 @@ def addhome():
             return "При добавлении возникла ошибка"
 
     else:
-        return render_template("AddHome.html")
+        return render_template("AddHome.html", current_year=current_year)
 
 
 @app.route('/about')
@@ -74,7 +92,8 @@ def posts_data(id):
     home = Home.query.get(id)
     bimg = b64encode(home.image)
     image = bimg.decode('utf-8')
-    return render_template("homedata.html", home=home, image=image)
+    address = Adress.query.filter_by(id=id).first()
+    return render_template("homedata.html", home=home, address=address, image=image)
 
 
 @app.route('/posts/<int:id>/delete')
@@ -82,6 +101,8 @@ def posts_delete(id):
     home = Home.query.get_or_404(id)
 
     try:
+        adress_data = Adress.query.filter_by(id=id).first()
+        db.session.delete(adress_data)
         db.session.delete(home)
         db.session.commit()
         return redirect('/posts')
@@ -92,23 +113,35 @@ def posts_delete(id):
 @app.route('/posts/<int:id>/edit', methods=['POST', 'GET'])
 def posts_edit(id):
     home = Home.query.get(id)
-    if request.method == "POST":
+    address = Adress.query.get(id)
+    bimg = b64encode(home.image)
+    image = bimg.decode('utf-8')
 
+    current_year = datetime.now().year
+
+    if request.method == "POST":
         home.year = request.form['year']
-        home.adress = request.form['adress']
         home.type = request.form['type']
         home.floor = request.form['floor']
         home.porch = request.form['porch']
 
-        try:
+        address.city = request.form['city']
+        address.district = request.form['district']
+        address.street = request.form['street']
+        address.homenum = request.form['homenum']
 
+        file = request.files.get('file')
+        if file:
+            home.filename = file.filename
+            home.image = file.read()
+
+        try:
             db.session.commit()
             return redirect('/posts')
         except:
             return "При изменении возникла ошибка"
     else:
-
-        return render_template("edit.html", home=home)
+        return render_template("edit.html", home=home, address=address, current_year=current_year, image=image)
 
 
 if __name__ == "__main__":
